75732e24fefe2ef74ed2ec06eeebc759c2ca5d47
Katie Burdett <katie.burdett@ao.com>
2025-05-29 11:56:52 +0100

    feat: adding basic props

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,14 +1,26 @@
-import { useState } from "react";
+import type { ReactNode } from "react";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <h1>Hello World</h1>
+      <Text title="Hi there!" subtitle="Welcome to our new site" />
     </>
   );
 }
 
+function Text({
+  title,
+  subtitle,
+}: {
+  title: React.ReactNode;
+  subtitle: React.ReactNode;
+}) {
+  return (
+    <div>
+      <h1>{title}</h1>
+      <p>{subtitle}</p>
+    </div>
+  );
+}
 export default App;
